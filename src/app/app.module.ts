fc57d22898119dcc8b9cc3ccfce9152734062b0a
leprashimabuku <shimabuku@lepra.jp>
2018-05-08 22:06:15 +0900

    「ng generate component theme」の実行と表示

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThemeComponent
   ],
   imports: [
     NgbModule.forRoot(),
